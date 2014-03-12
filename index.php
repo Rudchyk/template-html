dd1113179c15b3f5619c15c377206f18e5fa3535
Sergii Rudchyk <sergii.rudchyk@gmail.com>
2014-03-12 09:40:27 +0200

    init

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+    <meta charset="UTF-8">
+    <title>name</title>
+
+    <style>
+    .main{width: 940px;margin: auto;}
+    .img, h2{text-align: center}
+    .img{padding-bottom: 50px}
+    .img img{box-shadow: 0 0 15px #ccc;padding: 2px; max-width: 940px}
+    ul{width: 500px;margin: auto;padding-bottom: 30px}
+    a{padding-right: 20px;background: url(https://dl.dropboxusercontent.com/u/62557375/temp/external_link_icon.gif) no-repeat right}
+    </style>
+
+</head>
+<body>
+    <div class="main">
+        <h1>name</h1>
+        <ul>
+            <li><a href="index.html" target="_blank">index.html</a></li>
+            <li><a href="index.html" target="_blank">index.html</a></li>
+            <li><a href="index.html" target="_blank">index.html</a></li>
+        </ul>
+        <div class="img">
+            <img src="screenshot.jpg" alt="">
+        </div>
+    </div>
+</body>
+</html>
---
+++ js/init.js
@@ -0,0 +1,47 @@
+if (head.browser.ie && head.browser.version < 8) {
+    location.replace("ie7/ie7.html");
+}
+
+if (head.browser.ie && head.browser.version < 9) {
+    head.load("js/html5.js");
+}
+
+head.js(
+    "js/jquery-1.10.2.min.js",
+    "js/form.min.js",
+    "js/scripts.js",
+    function() {}
+);
+
+if (head.browser.ie && head.browser.version < 10) {
+    head.js(
+        "js/placeholder.min.js",
+        function() {
+            $("input[placeholder], textarea[placeholder]").textPlaceholder();
+        }
+    );
+}
+
+if (head.browser.opera) {
+    head.ready(document, function () {
+        function removePlaceholder(e) {
+            var el = e.target;
+            if (!el.placeHolderRemoved) {
+                el.placeHolderRemoved = true;
+                el.value = "";
+                el.removeAttribute("data-operaplaceholder");
+            }
+        }
+        var inputs = document.getElementsByTagName("input");
+        for (var i = 0; i < inputs.length; ++i) {
+            var el = inputs[i],
+                ph = el.getAttribute("placeholder");
+            if (ph && !el.hasAttribute("value")) {
+                el.value = ph;
+                el.removeAttribute("placeholder");
+                el.setAttribute("data-operaplaceholder", "");
+                el.addEventListener("click", removePlaceholder, false);
+            }
+        }
+    });
+}
