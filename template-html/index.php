f3e4654f22191a24048a38743bd23af3430cfbca
Sergii Rudchyk <sergii.rudchyk@gmail.com>
2014-03-17 16:43:27 +0200

    fix

--- template-html/index.php
+++ template-html/index.php
@@ -6,11 +6,12 @@
 
     <style>
     .main{width: 940px;margin: auto;}
-    .img, h2{text-align: center}
+    .img, h1{text-align: center}
     .img{padding-bottom: 50px}
     .img img{box-shadow: 0 0 15px #ccc;padding: 2px; max-width: 940px}
     ul{width: 500px;margin: auto;padding-bottom: 30px}
-    a{padding-right: 20px;background: url(https://dl.dropboxusercontent.com/u/62557375/temp/external_link_icon.gif) no-repeat right}
+    a{padding-right: 20px;background: url(https://dl.dropboxusercontent.com/u/62557375/temp/site/external_link_icon.gif) no-repeat right}
+    .psd{padding-right: 20px;background: url(https://dl.dropboxusercontent.com/u/62557375/temp/site/psd.png) no-repeat right}
     </style>
 
 </head>
@@ -18,9 +19,9 @@
     <div class="main">
         <h1>name</h1>
         <ul>
-            <li><a href="index.html" target="_blank">index.html</a></li>
-            <li><a href="index.html" target="_blank">index.html</a></li>
-            <li><a href="index.html" target="_blank">index.html</a></li>
+            <li><a href="index.html" target="_blank">index.html</a> (<span class="psd">PSD.psd</span>)</li>
+            <li><a href="index.html" target="_blank">index.html</a> (<span class="psd">PSD.psd</span>)</li>
+            <li><a href="index.html" target="_blank">index.html</a> (<span class="psd">PSD.psd</span>)</li>
         </ul>
         <div class="img">
             <img src="screenshot.jpg" alt="">
