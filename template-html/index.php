c10884f5cb2577f8119529e7ecdf69b455512508
Sergii Rudchyk <sergii.rudchyk@gmail.com>
2014-03-27 17:41:11 +0200

    fix index.php

--- template-html/index.php
+++ template-html/index.php
@@ -1,8 +1,11 @@
+<?php
+$templateName = 'mine-html';
+?>
 <!DOCTYPE HTML>
 <html lang="en-US">
 <head>
     <meta charset="UTF-8">
-    <title>index</title>
+    <title><?=$templateName?></title>
 
     <style>
     .main{width: 940px;margin: auto;}
@@ -11,17 +14,21 @@
     .img img{box-shadow: 0 0 15px #ccc;padding: 2px; max-width: 940px}
     ul{width: 500px;margin: auto;padding-bottom: 30px}
     a{padding-right: 20px;background: url(https://dl.dropboxusercontent.com/u/62557375/temp/site/external_link_icon.gif) no-repeat right}
-    .psd{padding-right: 20px;background: url(https://dl.dropboxusercontent.com/u/62557375/temp/site/psd.png) no-repeat right}
     </style>
 
 </head>
 <body>
     <div class="main">
-        <h1>name</h1>
+        <h1><?=$templateName?></h1>
         <ul>
-            <li><a href="index.html" target="_blank">index.html</a> (<span class="psd">PSD.psd</span>)</li>
-            <li><a href="index.html" target="_blank">index.html</a> (<span class="psd">PSD.psd</span>)</li>
-            <li><a href="index.html" target="_blank">index.html</a> (<span class="psd">PSD.psd</span>)</li>
+        <?php
+            if ($handle = opendir('.')) {
+                foreach (glob("*.html") as $filename) {
+                    echo "<li><a href='$filename' target='_blank'>$filename</a></li>";
+                }
+                closedir($handle);
+            }
+         ?>
         </ul>
         <div class="img">
             <img src="screenshot.jpg" alt="">
