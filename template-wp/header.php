3284afdf85c5bc36bf6058f33166bb5ddb369f51
Sergii Rudchyk <sergii.rudchyk@gmail.com>
2014-03-27 16:21:59 +0200

    fix

--- template-wp/header.php
+++ template-wp/header.php
@@ -6,11 +6,11 @@
     <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>">
     <meta name="description" content="<?php bloginfo('description'); ?>">
     <!-- CSS style -->
-    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" media="screen">
+    <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/index.css?v=1" media="screen">
     <!-- CSS for developers -->
     <!-- <link rel="stylesheet" href="<?php bloginfo('template_url');?>/css/dev.css?v=1" media="screen"> -->
     <!-- RSS -->
-    <link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="<?php bloginfo('rss2_url'); ?>">
+    <link rel="alternate" type="application/rss+xml" title="<?php _e('RSS 2.0', 'kubrick'); ?>" href="<?php bloginfo('rss2_url'); ?>">
     <!-- Scripts -->
     <script data-headjs-load="<?php bloginfo('template_url');?>/js/init.js" src="<?php bloginfo('template_url');?>/js/head.min.js"></script>
     <!--Подключает скрипт comment-reply.js на статической странице или странице поста. Скрипт перемещает форму добавления комментария под комментарий, у которого мы кликнули на ссылку “ответить” -->
